cf93e42f45fae5fe9b40ef6e3bbafd936d5d0a48
edsay12 <edvandearaujo2@hotmail.com>
2024-01-30 02:00:34 -0300

    Feat:CourseDescription component

--- src/app/course/[name]/page.tsx
+++ src/app/course/[name]/page.tsx
@@ -1,4 +1,5 @@
 import SectionContainer from "@/components/Section/SectionContainer";
+import Description from "@/components/Description";
 import Link from "next/link";
 import { AiFillStar } from "react-icons/ai";
 
@@ -45,16 +46,42 @@ function Course() {
           </div>
         </div>
       </SectionContainer>
-      <SectionContainer> 
-        <div className="grid grid-cols-4">
-
-          <div className="col-span-3  bg-red-400">
-            opaaaaa
+      <SectionContainer>
+        <div className="grid grid-cols-4 gap-5">
+          <div className="col-span-3 max-w-[900px]">
+            <div >
+              <Description title="Visão geral do curso">
+                Seja bem vindo(a) ao curso API Rest em NodeJS aplicando testes
+                (TDD) desde o início. Nesse curso vamos, inicialmente, aprender
+                a montar o nosso ambiente de desenvolvimento, configurar o
+                VSCode, e trabalhar com o lint para garantir a padronização dos
+                nossos códigos. Em seguida, apresentarei o básico do Jest para
+                criarmos os nossos testes... a partir daí vamos criar um teste e
+                desenvolver a nossa aplicação até o teste passar. E assim será
+                ao longo do curso, um teste de cada vez, uma funcionalidade de
+                cada vez, evoluindo sempre com segurança, pois configuraremos
+                nossos testes de tal forma que toda a API será testada sempre
+                que você salvar algum arquivo. Caso algo deixe de funcionar, com
+                um simples Ctrl+Z será possível retornar à normalidade de antes.
+                Durante o curso, iremos criar a API de um gerenciador financeiro
+                onde, no lado dos testes, apresentarei diversos recursos do Jest
+                para fazer assertivas e estruturar nossos testes. Já no lado do
+                desenvolvimento, trabalharemos com várias bibliotecas famosas
+                como o Express para criar o nosso servidor, o Passport e JWT
+                para autenticação, Knex e Postgres para migração e consultas ao
+                banco de dados, além de outras. Importante ressaltar que os
+                testes não serão a nível unitário, faremos todas as verificações
+                diretamente nos serviços. O que fará o teste mais real, pois o
+                fluxo passará desde a chamada da URL, passando pelo roteamento,
+                regras de negócio e banco de dados. Porém trará dificuldades
+                extras como a necessidade de possuir o ambiente sempre
+                atualizado e a necessidade de gerenciar a massa de dados
+                necessários para os testes, isso também será abordado no curso.
+                Abraços e nos vemos na próxima aula, até lá!
+              </Description>
+            </div>
           </div>
-          <div className="bg-blue-500 ">
-            opaaaaa
-          </div>
-
+          <div className="bg-blue-500 "></div>
         </div>
       </SectionContainer>
     </>
---
+++ src/components/Description.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { AiFillPlaySquare, AiFillPlusSquare } from "react-icons/ai";
+import Button from "./Button";
+import { FaLess, FaMinus, FaPlus } from "react-icons/fa";
+import { LegacyRef, useEffect, useRef, useState } from "react";
+
+type proptypes = {
+  title: string;
+  children: string;
+};
+
+function Description({ children, title = "aaaaaaaaaaaaaaaa" }: proptypes) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [showReadMoreButton, setShowReadMoreButton] = useState<boolean>(false);
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    
+    setShowReadMoreButton(
+      descriptionRef.current?.scrollHeight !=
+        descriptionRef.current?.clientHeight
+    );
+  }, [children]);
+
+  const toogleDescription = () => {
+    setIsOpen((state) => !state);
+  };
+  console.log(title);
+  return (
+    <>
+      <div className="w-full p-5 pt-0">
+        <div>
+          <h3 className="text-3xl text-gray-800 font-bold">{title}</h3>
+        </div>
+
+        <div
+          className={`  w-full mt-5 text-gray-600 ${
+            isOpen ? " " : "line-clamp-6"
+          } transition-all ease-in-out duration-500`}
+        >
+          <p className="" ref={descriptionRef}>
+            {children}
+          </p>
+        </div>
+        {showReadMoreButton && (
+          <div className="bg-gradient-to-b to-white from-transparent z-50 h-32 flex items-end -mt-20 relative">
+            <button
+              className="text-black mt-5 flex items-center gap-2 ml-4"
+              onClick={toogleDescription}
+            >
+              <span>{isOpen ? <FaMinus /> : <FaPlus />}</span>
+              {isOpen ? "Mostrar menos" : "Mostrar mais"}
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Description;
