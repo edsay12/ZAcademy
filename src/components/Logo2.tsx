7e991246cb62b13ad842d970d7f92df65c083b03
edsay12 <edvandearaujo2@hotmail.com>
2024-02-13 15:52:27 -0300

    feat : add logo2 component

---
+++ src/components/Logo2.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import logo2 from "../../public/Logo2.png";
+import Link from "next/link";
+import { twMerge } from "tailwind-merge";
+
+type PropTypes = {
+  className?: string;
+};
+
+function Logo2({ className }: PropTypes) {
+  return (
+    <Link href={"/"} >
+      <Image src={logo2} alt="Logo"  className={twMerge(className,'w-44')}/>
+    </Link>
+  );
+}
+
+export default Logo2;
