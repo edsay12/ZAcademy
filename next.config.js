dd3c99eb12c0ba0349fed10fa1ee14081a02a356
edsay12 <edvandearaujo2@hotmail.com>
2024-05-05 00:47:13 -0300

    feat: add router add new course

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
         }
     ]
   },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+      allowedForwardedHosts: ["localhost:3000"],
+      // ^ You might have to use this property depending on your exact version.
+    }
+  }
+  
 };
 
 module.exports = nextConfig;
--- src/app/api/courses/[userId]/route.ts
+++ src/app/api/courses/[userId]/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/lib/db";
+import { Course } from "@prisma/client";
 
 import { NextRequest, NextResponse } from "next/server";
 
@@ -6,20 +7,85 @@ export async function GET(
   req: NextRequest,
   { params }: { params: { userId: string } }
 ) {
-  
-  const {userId} = params
+  const { userId } = params;
 
+  const isUserExists = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
 
-  if (!userId) {
+  if (!isUserExists) {
     return NextResponse.json({ data: "user id not found" }, { status: 400 });
   }
   try {
-    const couses = db.course.findMany({
+    const couses = await db.course.findMany({
       where: {
         userId: userId,
       },
     });
     return NextResponse.json({ data: couses }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ data: error }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { userId: string } }
+) {
+  const { userId } = params;
+  const data = await req.json();
+
+  const {
+    title,
+    description,
+    category,
+    presentationVideo,
+    level,
+    price,
+    image,
+  } = data as Course;
+
+  const isUserExists = await db.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (
+    !title ||
+    !description ||
+    !category ||
+    !presentationVideo ||
+    !level ||
+    !price
+  ) {
+    return NextResponse.json({ error: "Dados Invalidos" }, { status: 400 });
+  }
+
+  if (!isUserExists) {
+    return NextResponse.json({ data: "user id not found" }, { status: 400 });
+  }
+
+  try {
+    const couse = await db.course.create({
+      data:{
+        title:title,
+        userId:userId,
+        category:category,
+        image:image,
+        description:description,
+        level:level,
+        presentationVideo:presentationVideo,
+        price:price,
+        starNumber:0,
+        assessmentsNumber:0,
+        studentsNumber:0
+      }
+    });
+    console.log(couse);
+    return NextResponse.json({ data: couse }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ data: error }, { status: 500 });
   }
